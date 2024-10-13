8eda57a7c3759ed88dcc39fa8fff52a16e632828
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-10-14 01:18:28 +0700

    Menambahkan fungsi Perulangan, dengan contoh for, while, dan do while loop

--- app/src/androidTest/java/com/dev/myapp/ConditionalExpression.kt
+++ app/src/androidTest/java/com/dev/myapp/ConditionalExpression.kt
@@ -24,7 +24,6 @@ fun ConditionalStatement(){
         else -> println("Invalid Day")
     }
 
-
     // Try-Catch
     val value = "10"
     try {
@@ -33,5 +32,31 @@ fun ConditionalStatement(){
     } catch (e: NumberFormatException) {
         println("Invalid Number")
     }
+}
+
+fun Perulangan() {
+    println()
+    println("=== Perulangan ===")
+
+    for (i in 1..5) {
+        println("Perulangan ke-$i")
+    }
+
+    val cars = listOf("BMW", "Toyota", "Honda", "Audi")
+    for ( car in cars) {
+        println("Car type: $car")
+    }
+
+    var x = 1
+    while (x <= 5) {
+        println("Perulangan ke-$x")
+        x++
+    }
+    var y = 1
+    do{
+        println("Perulangan ke-$y")
+        y++
+    }while (y <= 5)
+
 }
 
