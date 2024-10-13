edb603b85a5f9da2359a0a619f5e1bfe17c3d159
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-10-14 00:34:19 +0700

    Menambahkan contoh penggunaan map

--- app/src/androidTest/java/com/dev/myapp/Collections.kt
+++ app/src/androidTest/java/com/dev/myapp/Collections.kt
@@ -50,3 +50,31 @@ fun ContohSet(){
     println(shapesLocked)
 }
 
+fun ContohMap() {
+    println()
+    println("=== Map ===")
+    // Map Read-Only
+    val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(readOnlyShape)
+
+    //Map Mutable
+    val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
+    // Menambahkan data ke dalam Map Mutable
+    shape["Rectangle"] = 4
+    println(shape)
+
+    // Menghapus data dari Map Mutable
+    shape.remove("Circle")
+    println(shape)
+
+    // Menghapus data dari Map Mutable
+    shape["Square"] = 5
+    println(shape)
+
+    // Map Read-Only
+    val shapesLocked: Map<String, Int> = shape
+    println(shapesLocked)
+}
+
